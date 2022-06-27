01c91de9b55a924c8deb7e7ab93132e5969bd7d4
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-27 12:53:49 -0300

    test: 💍 create custom inputs with invalid requests

--- src/tests/inputs/todoInputs.ts
+++ src/tests/inputs/todoInputs.ts
@@ -6,3 +6,25 @@ export const todoValidCreate = {
   conclusion_date: '2022/06/05',
 };
 
+export const todoInvalidCreate1 = {
+  execution_date: '2022/06/01',
+  situation: 'Pendente',
+  priority: 'Alta',
+  conclusion_date: '2020/06/05',
+};
+
+export const todoInvalidCreate2 = {
+  name: 'Tarefa 3',
+  execution_date: "2020-01-01T00:00:00.000Z",
+  situation: 'Pendente',
+  priority: 'Alta',
+  conclusion_date: '13/12/1992',
+};
+
+export const todoInvalidCreate3 = {
+  name: 'Tarefa 3',
+  execution_date: '2022/06/01',
+  situation: 'Cancelado',
+  priority: 'Alta',
+  conclusion_date: '2022/06/05',
+};
