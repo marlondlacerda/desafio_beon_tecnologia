c698f3d787313fa916b113f6bf70b2a0ec546fcc
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-27 12:30:23 -0300

    refactor: 💡 create a new method of class responseError

--- src/middlewares/Validation.ts
+++ src/middlewares/Validation.ts
@@ -2,6 +2,9 @@ import { APIGatewayEvent } from 'aws-lambda';
 import { Schema, ZodError } from 'zod';
 import { CreateError, MessageUtil } from '../utils';
 
+const DATE_MSG = '"execution_date" deve ser uma data! exemplo:'
+  + '"YY/MM/DD" ou "2020-01-01T00:00:00.000Z"';
+
 class Validation {
   constructor(
     private readonly bodySchema: Schema<unknown>,
@@ -12,11 +15,7 @@ class Validation {
     try {
       this.bodySchema.parse(JSON.parse(event.body));
     } catch (err: unknown) {
-      if (err instanceof ZodError) {
-        const { message } = err.issues[0];
-        const error = CreateError('badRequest', message);
-        return MessageUtil.error(error);
-      }
+      return Validation.responseError(err);
     }
   };
 
@@ -24,13 +23,23 @@ class Validation {
     try {
       this.paramsSchema.parse(event.pathParameters);
     } catch (err: unknown) {
-      if (err instanceof ZodError) {
-        const { message } = err.issues[0];
-
-        const error = CreateError('badRequest', message);
-        return MessageUtil.error(error);
-      }
+      return Validation.responseError(err);
     }
+  };
+
+  static responseError = (error: unknown) => {
+    if (error instanceof ZodError) {
+      const { message } = error.issues[0];
+
+      const result = message === 'Invalid date' 
+        ? CreateError('badRequest', DATE_MSG) 
+        : CreateError('badRequest', message);
+
+      return MessageUtil.error(result);
+    }
+
+    const err = CreateError('error', 'Internal server error');
+    return MessageUtil.error(err);
   };
 }
 
