0ed9e52854a60fbe54fa58edf9683d89256cfd50
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-27 11:45:26 -0300

    test: 💍 refactor tests and create inputs

---
+++ src/tests/inputs/todoInputs.ts
@@ -0,0 +1,8 @@
+export const todoValidCreate = {
+  name: 'Tarefa 3',
+  execution_date: '2022/06/01',
+  situation: 'Pendente',
+  priority: 'Alta',
+  conclusion_date: '2022/06/05',
+};
+
--- src/tests/todo.test.ts
+++ src/tests/todo.test.ts
@@ -2,49 +2,42 @@ import lambdaTester from 'lambda-tester';
 import { expect } from 'chai';
 import { create } from '../handler';
 import * as todosMock from './mocks/todo.mocks';
+import * as todosInput from './inputs/todoInputs';
 import { TodoModel } from '../models/';
 
 import sinon from 'sinon';
 const todoModel = new TodoModel();
 
-describe('Create [POST]', () => {
-  it('success', () => {
-    const s = sinon.stub(todoModel.model, 'create').resolves(todosMock.create);
+describe('1) - Create [POST]', () => {
+  describe('1) - When the request is valid', () => {
+    it('1) - When success', () => {
+      const s = sinon.stub(todoModel.model, 'create').resolves(todosMock.create);
 
-    return lambdaTester(create)
-      .event({ body: JSON.stringify({
-        name: 'Tarefa 3',
-        execution_date: '2022/06/01',
-        situation: 'Pendente',
-        priority: 'Alta',
-        conclusion_date: '2022/06/05',
-      })})
-      .expectResult((result: any) => {
-        const body = JSON.parse(result.body);
-        
-        expect(result.statusCode).to.equal(201);
-        expect(body).to.deep.equal(todosMock.create);
-        s.restore();
-      });
+      return lambdaTester(create)
+        .event({ body: JSON.stringify(todosInput.todoValidCreate) })
+        .expectResult((result: any) => {
+          const body = JSON.parse(result.body);
+          
+          expect(result.statusCode).to.equal(201);
+          expect(body).to.deep.equal(todosMock.create);
+          s.restore();
+        });
+    });
   });
 
-  it('error', () => {
-    const s = sinon.stub(todoModel.model, 'create').rejects(todosMock.castError);
+  describe('2) - When the request is invalid or Error', () => {
+    it('1) - When Error', () => {
+      const s = sinon.stub(todoModel.model, 'create').rejects(todosMock.castError);
 
-    return lambdaTester(create)
-      .event({ body: JSON.stringify({
-        name: 'Tarefa 3',
-        execution_date: '2022/06/01',
-        situation: 'Pendente',
-        priority: 'Alta',
-        conclusion_date: '2022/06/05',
-      })})
-      .expectResult((result: any) => {
-        const body = JSON.parse(result.body);
+      return lambdaTester(create)
+      .event({ body: JSON.stringify(todosInput.todoValidCreate) })
+        .expectResult((result: any) => {
+          const body = JSON.parse(result.body);
 
-        expect(result.statusCode).to.equal(400);
-        expect(body).to.deep.equal(todosMock.resultCreateError);
-        s.restore();
-      });
+          expect(result.statusCode).to.equal(400);
+          expect(body).to.deep.equal(todosMock.resultCreateError);
+          s.restore();
+        });
+    });
   });
 });
