79589083233aeeec26a83869816891b1f2b3db68
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-06-27 09:20:49 -0300

    test: 💍 creating mock files for tests

---
+++ src/tests/mocks/todo.mocks.ts
@@ -0,0 +1,43 @@
+export const findOne = {
+  "_id": "5dff58da85eb210f0aac43af",
+  "name": "Tarefa 1",
+  "execution_date": "2022/06/01",
+  "situation": "Pendente",
+  "priority": "Alta",
+  "conclusion_date": "2020/06/05",
+};
+
+export const find = [
+  {
+    "_id": "5dff58da85eb210f0aac43af",
+    "name": "Tarefa 1",
+    "execution_date": "2022/06/01",
+    "situation": "Pendente",
+    "priority": "Alta",
+    "conclusion_date": "2020/06/05",
+  },
+  {
+    "_id": "5e0188f53877986a548aa6f4",
+    "name": "Tarefa 2",
+    "execution_date": "2022/06/08",
+    "situation": "Concluída",
+    "priority": "Baixa",
+    "conclusion_date": "2020/06/10",
+  }
+];
+
+export const create = {
+  "name": "Tarefa 3",
+  "execution_date": "2022/06/01",
+  "situation": "Pendente",
+  "priority": "Alta",
+  "conclusion_date": "2020/06/05",
+};
+
+export const update = {
+  "name": "Tarefa 3",
+  "execution_date": "2022/06/01",
+  "situation": "Concluída",
+  "priority": "Alta",
+  "conclusion_date": "2020/06/05",
+};
